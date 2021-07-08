d70dcbf74527f597f00d424709dc5228317bb5aa
LNY <1162255758@qq.com>
2021-07-08 15:39:16 +0800

    Guava异步回调

--- demo123456/src/main/java/com/example/demo123456/Emun/exceCodeAll.java
+++ demo123456/src/main/java/com/example/demo123456/Emun/exceCodeAll.java
@@ -66,7 +66,7 @@ public enum exceCodeAll implements codeMessage {
         this.code = code;
     }
 
-    public void setMessage(String message) {
+    public void setMessage(String  message) {
         this.message = message;
     }
 
